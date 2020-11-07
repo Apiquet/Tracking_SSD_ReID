1d03d3b9a2ba23d3c2be507cdaea6fc66e2ebdd6
Anthony <anthony.piquet2@gmail.com>
2020-11-07 16:22:46 +0100

    [DEV][DB] Create DBManager class

---
+++ models/DBManager.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Pascal VOC2012 dataset manager
+"""
+
+import numpy as np
+
+
+class DBManager():
+
+    def __init__(self):
+        super(DBManager, self).__init__()
+        
+
+    def call(self, x):
+        return self.model(x)
