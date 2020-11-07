0073999ae09bb94aefc80d52b2cf5ae851934f33
Anthony <anthony.piquet2@gmail.com>
2020-11-07 17:08:08 +0100

    [DEV][DB] Add method to get train val df

--- models/DBManager.py
+++ models/DBManager.py
@@ -5,14 +5,26 @@
 Pascal VOC2012 dataset manager
 """
 
-import numpy as np
+import pandas as pd
 
 
 class DBManager():
 
     def __init__(self):
         super(DBManager, self).__init__()
-        
 
-    def call(self, x):
-        return self.model(x)
+    def getTrainValDfClass(self, path, class_name=""):
+        train_filepath = path + '/ImageSets/Main/' + class_name + "_train.txt"
+        val_filepath = path + '/ImageSets/Main/' + class_name + "_val.txt"
+
+        train_df = pd.read_csv(train_filepath, delimiter=r"\s+", header=None)
+        train_df.columns = ["ImgName", "Value"]
+        train_df = train_df.drop(train_df[train_df.values == -1].index)
+
+        val_df = pd.read_csv(val_filepath, delimiter=r"\s+", header=None)
+        val_df.columns = ["ImgName", "Value"]
+        val_df = val_df.drop(val_df[val_df.values == -1].index)
+
+        return train_df, val_df
+
+        
