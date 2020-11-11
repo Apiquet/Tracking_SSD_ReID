c58bdf008d9cc2620d906f5cf2a6115b4312cea5
Anthony <anthony.piquet2@gmail.com>
2020-11-11 18:45:43 +0100

    [DEv][DB] Get batch images in tensor

--- data_management/VOC2012ManagerObjDetection.py
+++ data_management/VOC2012ManagerObjDetection.py
@@ -5,10 +5,9 @@
 Pascal VOC2012 dataset manager
 """
 
-import pandas as pd
 import numpy as np
 import os
-from shutil import copyfile
+import tensorflow as tf
 
 
 class VOC2012ManagerObjDetection():
@@ -23,3 +22,17 @@ class VOC2012ManagerObjDetection():
                         'sheep', 'sofa', 'train', 'tvmonitor']
         self.images_path = path + "/JPEGImages/"
         self.annotations_path = path + "/Annotations/"
+        self.images_name = [im.replace(".jpg", "")
+                            for im in os.listdir(self.images_path)
+                            if os.path.isfile(os.path.join(self.images_path,
+                                                           im))]
+        self.number_samples = len(self.images_name)
+
+    def getImages(self, images_name: list):
+        images = []
+        for img in images_name:
+            image = tf.keras.preprocessing.image.load_img(
+                self.images_path + img + ".jpg",
+                target_size=self.img_resolution)
+            images.append(tf.keras.preprocessing.image.img_to_array(image))
+        return tf.convert_to_tensor(images, dtype=tf.float32)
